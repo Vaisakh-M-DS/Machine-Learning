9f6bfd0167691994b5154a5755f3322285a1e0ee
Vaisakh-M-DS <vaisakh491@gmail.com>
2025-02-26 16:44:54 +0530

    Removed AI_Cybersecurity from Machine-Learning repository

--- AI_Cybersecurity/read_ports.py
+++
@@ -1,9 +0,0 @@
-# Open and read the file
-with open("common-http-ports.txt", "r") as file:
-    ports = file.readlines()
-
-# Remove extra spaces and newlines
-ports = [port.strip() for port in ports]
-
-# Print first few ports
-print("Common HTTP Ports:", ports[:10])  # Display first 10 ports
